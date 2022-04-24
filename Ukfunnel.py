45def6d0c0c896b68e5ff992f0df4b85051dc25e
Siddhi <siddhishah1993@gmail.com>
2022-04-24 18:54:31 +0530

    valid xpath

--- Ukfunnel.py
+++ Ukfunnel.py
@@ -1,17 +1,32 @@
 # from multiprocessing.connection import wait
+import time
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
-
-driver = webdriver.Chrome("C:/UK Retail/Uk-region/chromedriver.exe")
+driver = webdriver.Chrome(executable_path="C:/UK Retail/Uk-region/chromedriver.exe")
+driver.maximize_window()
 driver.get("https://uat.travelex-dev.net/gb/purchase")  # load the url
-
 cashpassport = driver.find_element_by_id("#cashpassport").click()
-# wait 10 seconds before looking for element
+iframe = driver.find_elements_by_tag_name('iframe')[1]
+driver.switch_to.frame(iframe)
+driver.find_element_by_name("credit-card-number").send_keys("5384360000013451")
+driver.implicitly_wait(20)
+driver.find_element_by_xpath("//*[@id='noItems']/div/form/div/div[2]/div[3]/button").click()
 
-try:
-    driver.find_element_by_xpath("//input[@name='credit-card-number']").send_keys("5384360000013451")
+# card_number.send_keys(Keys.TAB)
+# card_number.send_keys(Keys.ENTER)
 
-except NoSuchElementException:
-    pass
+
+
+
+
+# # wait 10 seconds before looking for element
+#
+# time.sleep(5)
+# try:
+#     driver.find_element_by_xpath("//input[@name='credit-card-number']").send_keys("5384360000013451")
+#
+# except NoSuchElementException:
+#     pass
