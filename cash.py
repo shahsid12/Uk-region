d9cf291b09fe33efc7ea22ff165d9dd7753931cd
Siddhi <siddhishah1993@gmail.com>
2022-05-01 21:21:31 +0530

    valid xpath

--- cash.py
+++ cash.py
@@ -7,7 +7,7 @@ driver = webdriver.Chrome("C:/UK Retail/Uk-region/chromedriver.exe")
 driver.get("https://uat.travelex-dev.net/gb/purchase")
 driver.maximize_window()
 # driver.find_element_by_class_name('autocomplete-holder').click()
-time.sleep(2)
+# time.sleep(2)
 # currency = driver.find_elements_by_css_selector("li[class='ui-menu-item'] a")
 # print(len(currency))
 
@@ -15,18 +15,26 @@ time.sleep(2)
 #     if money.text == "Australia - Australian Dollar (AUD)":
 #         money.click()
 #         break
-
+driver.find_element_by_class_name("autocomplete-holder").click()
+# driver.find_element_by_id("ui-id-4")
+currencies = []
+currencies = driver.find_elements_by_xpath(".//*[@id='ui-id-4']/li")
+for item in currencies:
+    print(item.text)
+    if item.text == "Canada - Canadian Dollar (CAD)":
+        item.click()
+        break
+# time.sleep(2)
 amount = driver.find_element_by_id("noItems-buy-amount")
 amount.send_keys(Keys.CONTROL + "a")
 amount.send_keys(Keys.DELETE)
 amount.send_keys("500")
 time.sleep(2)
 buy_cash = driver.find_element_by_css_selector("#btnBuyCash").click()
+time.sleep(5)
+driver.execute_script("window.scrollTo(0,850)")
+time.sleep(3)
 
-driver.execute_script("driver.execute_script("")")
-home_delivery = driver.find_element_by_link_text("#homeDelivery")
-home_delivery.click()
-
-# time.sleep(30)
-# driver.close()
-# shahsid12
+home_deliver = driver.find_element_by_xpath(".//*[@class='button delivery buying-options-tab tabnav-3']").click()
+driver.implicitly_wait(10)
+driver.close()
